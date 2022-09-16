30b119ae4dbf7b5e31b601f671128e10c140f9eb
fatihyanik <fatihyanik07@gmail.com>
2022-09-16 17:28:59 +0200

    finish chat app

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAwqi5XAt0Rc5biFQbQgflB7ERDjPFpWNQ",
-    authDomain: "chatapp-f51fc.firebaseapp.com",
-    projectId: "chatapp-f51fc",
-    storageBucket: "chatapp-f51fc.appspot.com",
-    messagingSenderId: "995925946929",
-    appId: "1:995925946929:web:fec23c68410bca62abdb10"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+  };
+  
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
